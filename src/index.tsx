2b35c16560616a62435537ad346d6de356af9d17
Leandro Lopes <leandro-lopes46@hotmail.com>
2022-05-21 17:00:45 -0300

    feat: add theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import { ThemeProvider } from 'styled-components'
+
+import { GlobalStyles } from 'styles/global'
+import { theme } from 'styles/theme'
+
 import { App } from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root')!)
 root.render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
     <App />
-  </React.StrictMode>,
+    <GlobalStyles />
+  </ThemeProvider>,
 )
---
+++ src/styles/theme.ts
@@ -0,0 +1,35 @@
+export const theme = {
+  colors: {
+    primary: '#546DD8',
+    background: '#F6F6F6',
+    black: '#232324',
+    white: '#FFFFFF',
+    neutral: {
+      200: '#ABABAB',
+      300: '#576061',
+    },
+  },
+  font: {
+    sizes: {
+      small: '1.6rem',
+      medium: '2rem',
+      large: '2.4rem',
+      xlarge: '3.2rem',
+      huge: '5rem',
+    },
+    family: {
+      logo: 'Itim, cursive',
+      main: 'Open Sans, sans-serif',
+    },
+  },
+  transition: {
+    slow: '0.5s ease-in-out',
+    default: '0.3s ease-in-out',
+    fast: '0.1s ease-in-out',
+    ease: {
+      slow: '0.5s ease',
+      default: '0.25s ease',
+      fast: '0.1s ease',
+    },
+  },
+} as const
