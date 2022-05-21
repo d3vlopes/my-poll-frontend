b62bda9ea738693b42174572b665446ea0a8b396
Leandro Lopes <leandro-lopes46@hotmail.com>
2022-05-21 17:00:25 -0300

    feat: add global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    background: ${({ theme }) => theme.colors.background};
+    color: ${({ theme }) => theme.colors.black};
+    -webkit-font-smoothing: antialiased;
+    font-size: ${({ theme }) => theme.font.sizes.small};
+  }
+
+  body, input, textarea, button { 
+    font-family: ${({ theme }) => theme.font.family.main};
+  }
+
+  button {
+    cursor: pointer;
+  }
+`
